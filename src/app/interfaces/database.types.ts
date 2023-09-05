e4c77925bb77564ce969052f4bb619140347076c
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-09-05 13:50:50 -0300

    feat: Adicionando o Supabase e as devidas configs

---
+++ src/app/interfaces/database.types.ts
@@ -0,0 +1,65 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      products: {
+        Row: {
+          aditional: Json[] | null
+          category: string[]
+          color: string[]
+          context: string
+          description: string | null
+          id: number
+          image: string[]
+          manual: string | null
+          name: string
+          price: number | null
+        }
+        Insert: {
+          aditional?: Json[] | null
+          category: string[]
+          color: string[]
+          context: string
+          description?: string | null
+          id?: never
+          image: string[]
+          manual?: string | null
+          name: string
+          price?: number | null
+        }
+        Update: {
+          aditional?: Json[] | null
+          category?: string[]
+          color?: string[]
+          context?: string
+          description?: string | null
+          id?: never
+          image?: string[]
+          manual?: string | null
+          name?: string
+          price?: number | null
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
